fcc0894b3967ea456692948525c94d5d60813d8d
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-06-07 17:26:47 +0300

    Pdf Report Added

--- WebAPI/DatabaseInit/Program.cs
+++ WebAPI/DatabaseInit/Program.cs
@@ -1,11 +1,6 @@
-﻿using MovieDb.Data;
-using MovieDb.Data.Migrations;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using PDFReportGeneration;
+
 
 namespace DatabaseInit
 {
@@ -13,9 +8,10 @@ namespace DatabaseInit
     {
         static void Main(string[] args)
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MoviesContext, ConfigurationMovieDB>());
-            var db = new MoviesContext();
-            db.Adresses.ToList();
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<MoviesContext, ConfigurationMovieDB>());
+            //var db = new MoviesContext();
+            //db.Adresses.ToList();
+            PdfReporter.GeneratePDFAggregatedSalesReport();
         }
     }
 }
