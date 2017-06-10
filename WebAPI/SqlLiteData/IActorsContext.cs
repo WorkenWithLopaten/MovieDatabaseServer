7711b64dd9e07b8e535f8e320ff7a3faea8ff044
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-06-10 16:30:45 +0300

    Ready for migration

---
+++ WebAPI/SqlLiteData/IActorsContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieDb.Data
+{
+    using System.Data.Entity;
+    using SqlLiteData.Models;
+    public interface IActorsContext
+    {
+        DbSet<MoviesLite> Movies { get; set; }
+        DbSet<Actors> Actors { get; set; }
+        void Dispose();
+        int SaveChanges();
+    }
+}
