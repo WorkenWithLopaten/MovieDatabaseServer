5c278a0ac4c40651f181616caf5bf2c724f0b5f5
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-06-11 09:07:26 +0300

    before migrations

---
+++ WebAPI/PostgreData/Models/Cinema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreData.Models
+{
+    public class Cinema
+    {
+        private ICollection<CinemaCity> cities;
+
+        public Cinema()
+        {
+            this.Cities = new HashSet<CinemaCity>();
+        }
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        public virtual ICollection<CinemaCity> Cities
+        {
+            get { return this.cities; }
+            set { this.cities = value; }
+        }
+    }
+}
---
+++ WebAPI/PostgreData/Models/CinemaCity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreData.Models
+{
+    public class CinemaCity
+    {
+        private ICollection<Cinema> cinemas;
+
+        public CinemaCity()
+        {
+            this.Cinemas = new HashSet<Cinema>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Cinema> Cinemas
+        {
+            get { return this.cinemas; }
+            set { this.cinemas = value; }
+        }
+    }
+}
